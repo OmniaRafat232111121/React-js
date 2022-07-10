c986e675a3929061498948bda5e9b2f55f5d3b7b
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2022-07-10 02:35:20 +0200

    lists

--- .history/src/index_20220710013853.js
+++ .history/src/index_20220710013853.js
@@ -600,16 +600,6 @@
 // root.render(<FavoriteColor />);
 
 
-// import { useState } from "react";
-// import ReactDOM from "react-dom/client";
 
-// function FavoriteColor() {
-//   const [color, setColor] = useState("red");
-
-//   return <h1>My favorite color is {color}!</h1>
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<FavoriteColor />);
 
 
--- .history/src/index_20220710013854.js
+++ .history/src/index_20220710013854.js
@@ -600,16 +600,16 @@
 // root.render(<FavoriteColor />);
 
 
-// import { useState } from "react";
-// import ReactDOM from "react-dom/client";
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-// function FavoriteColor() {
-//   const [color, setColor] = useState("red");
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
 
-//   return <h1>My favorite color is {color}!</h1>
-// }
+  return <h1>My favorite color is {color}!</h1>
+}
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<FavoriteColor />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<FavoriteColor />);
 
 
